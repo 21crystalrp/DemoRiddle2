9da4f35f6f3e51c2e1bf60486e3dc09603db2598
LAI KE EN <21033293@myrp.edu.sg>
2023-05-17 15:57:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21033293/demoriddle/AnswerActivity2.java
@@ -0,0 +1,56 @@
+package sg.edu.rp.c346.id21033293.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AnswerActivity2 extends AppCompatActivity {
+
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer2);
+
+        Log.d("MainActivity", "onCreate() called.");
+
+        tvAnswer = findViewById(R.id.textView2);
+        Intent intentReceived = getIntent();
+        String questionsSelected = intentReceived.getStringExtra("Question");
+        tvAnswer.setText(questionsSelected + " answer is: Gone");
+    }
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
+}
